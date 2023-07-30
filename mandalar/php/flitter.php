a6877cb2f3cf17cb0360b2e7cbb0796b57a6bb5e
yuzana <34826444+yazanu@users.noreply.github.com>
2023-07-30 14:38:07 +0630

    image add in product array

--- mandalar/php/flitter.php
+++ mandalar/php/flitter.php
@@ -4,6 +4,16 @@ include_once __DIR__."/../model/post.php";
 if(isset($_POST['flitteringData'])){
     $Post_Modal = new Post();
     $flitteringData = json_decode($_POST['flitteringData']) ;
-    $FliteringResult = json_encode($Post_Modal->getPostByFlitter($flitteringData));
-    echo $FliteringResult;
+    $FliteringResult = $Post_Modal->getPostByFlitter($flitteringData);
+    $FliteringResultWithImage = [];
+
+    foreach ($FliteringResult as $key => $value) {
+        // var_dump($value['photo_folder']);
+        $images = glob('../image/post_img/' . $value['photo_folder'] . '/*.{jpg,png,gif,jfifx  }', GLOB_BRACE);
+        // var_dump($images);
+        $value += array('product_image'=>$images[0]);
+        $FliteringResultWithImage[] = $value;
+    }
+ 
+    echo json_encode($FliteringResultWithImage);
 }
