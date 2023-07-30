c318bd6c387d7df8f506519ed183ceb1144c7062
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-07-30 23:35:21 +0630

    comment created function add

--- mandalar/model/comment.php
+++ mandalar/model/comment.php
@@ -10,8 +10,22 @@ class Comment
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->table = 'comment';
     }
-    function createComment(){
-        $sql = "";
+    function createComment($postId,$userId,$content,$ParentcommentId)
+    {
+        $sql = "INSERT INTO 
+        `$this->table`( `post_id`, `user_id`, `content`, `parent_com_id`)
+         VALUES (:post_id, :user_id , :content, :parent_cm_id )";
+        $statement = $this->connection->prepare($sql);
+        $statement->bindParam(':post_id',$postId);
+        $statement->bindParam(':user_id',$userId);
+        $statement->bindParam(':content',$content);
+        $statement->bindParam(':parent_cm_id',$ParentcommentId);
+        if($statement->execute()){
+            echo "Comment Created Successfully";
+        }
+        else{
+            echo "Comment Created Err";
+        }
     }
 
 }
