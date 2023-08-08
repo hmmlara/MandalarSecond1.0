0898ed68393bea9af5c9fa9d539c6f2045d88da9
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-08-08 10:15:12 +0630

    error

--- mandalar/js/comment.js
+++ mandalar/js/comment.js
@@ -10,7 +10,6 @@ let PostId = postIdStoreBox.dataset.postId;
 let ParentCommentId = null;
 let Content = "";
 
-
 //Assign Content Value From Input
 commentInput.addEventListener("keyup", (e) => {
 	Content = e.target.value;
@@ -46,7 +45,6 @@ const loadComments = () => {
 		success: function (data) {
 			let CommentList;
 			let response = JSON.parse(data);
-
 			response.forEach((element) => {
 				CommentList += `
                      <div class ="comment' >
@@ -56,50 +54,180 @@ const loadComments = () => {
                                         <div class="comment-content">
                                             <div class="comment-details">
                                                 <span class="comment-author">${element.name}</span>
-                                                <span class="comment-date">Posted on </span>
+                                                <span class="comment-date">Posted on ${element.date}</span>
                                             </div>
                                             ${element.content}
                                         </div>
                                         <div class="comment-actions">
-                                            <button class="btn btn-link btn-sm">Like</button>
                                             <button class="btn btn-link btn-sm" onclick="assignParentId(event)" data-cm-id = ${element.id}>Reply</button>
-                                            <button class="btn btn-link btn-sm see-reply" onclick="seeReplies(event)">see 2
+                                            <button class="btn btn-link btn-sm see-reply" onclick="seeReplies(event)">see replies
 
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="replies">
-                                </div> 
-                        </div>`;
-			});
-			if (normalizeWhitespace(CommentList) !== normalizeWhitespace(commentContainer.innerHTML)) {
-				// Your logic here
-				commentContainer.innerHTML = CommentList;
-				console.log( normalizeWhitespace(CommentList))
-				console.log(normalizeWhitespace(commentContainer.innerHTML))
-				console.log("reassign")
-			  }
-		
+                            `;
+				$.ajax({
+					url: "php/load_replies.php",
+					type: "POST",
+					data: { parent_comment_id: element.id },
+					success: function (data) {
+						let result = JSON.parse(data);
+						console.log(result)
+						result.forEach((element) => {
+							CommentList += `
+								 <div class ="comment' >
+											<div class="d-flex">
+												<img src="image/user-profile/${element.img}"  class="profile-picture-comment" alt="${element.img}" style="width: max-content" />
+												<div>
+													<div class="comment-content">
+														<div class="comment-details">
+															<span class="comment-author">${element.name}</span>
+															<span class="comment-date">Posted on ${element.date}</span>
+														</div>
+														${element.content}
+													</div>
+													<div class="comment-actions">
+														<button class="btn btn-link btn-sm">Like</button>
+														<button class="btn btn-link btn-sm" onclick="assignParentId(event)" data-cm-id = ${element.id}>Reply</button>
+														<button class="btn btn-link btn-sm see-reply" onclick="seeReplies(event)">see 2
+			
+													</div>
+												</div>
+											</div>
+			
+											<div class="replies">
+										`;
+							$.ajax({
+								url: "php/load_replies.php",
+								type: "POST",
+								data: { parent_comment_id: element.id },
+								success: function (data) {
+									let result = JSON.parse(data);
+									result.forEach((element) => {
+										CommentList += `
+											 <div class ="comment' >
+														<div class="d-flex">
+															<img src="image/user-profile/${element.img}"  class="profile-picture-comment" alt="${element.img}" style="width: max-content" />
+															<div>
+																<div class="comment-content">
+																	<div class="comment-details">
+																		<span class="comment-author">${element.name}</span>
+																		<span class="comment-date">Posted on ${element.date}</span>
+																	</div>
+																	${element.content}
+																</div>
+																<div class="comment-actions">
+																	<button class="btn btn-link btn-sm">Like</button>
+																	<button class="btn btn-link btn-sm" onclick="assignParentId(event)" data-cm-id = ${element.id}>Reply</button>
+																	<button class="btn btn-link btn-sm see-reply" onclick="seeReplies(event)">see 2
+						
+																</div>
+															</div>
+														</div>
+						
+														<div class="replies">
+													`;
+										$.ajax({
+											url: "php/load_replies.php",
+											type: "POST",
+											data: { parent_comment_id: element.id },
+											success: function (data) {
+												let result = JSON.parse(data);
+												result.forEach((element) => {
+													CommentList += `
+														 <div class ="comment' >
+																	<div class="d-flex">
+																		<img src="image/user-profile/${element.img}"  class="profile-picture-comment" alt="${element.img}" style="width: max-content" />
+																		<div>
+																			<div class="comment-content">
+																				<div class="comment-details">
+																					<span class="comment-author">${element.name}</span>
+																					<span class="comment-date">Posted on ${element.date}</span>
+																				</div>
+																				${element.content}
+																			</div>
+																			<div class="comment-actions">
+																				<button class="btn btn-link btn-sm">Like</button>
+																				<button class="btn btn-link btn-sm" onclick="assignParentId(event)" data-cm-id = ${element.id}>Reply</button>
+																				<button class="btn btn-link btn-sm see-reply" onclick="seeReplies(event)">see 2
+									
+																			</div>
+																		</div>
+																	</div>
+									
+																	<div class="replies">
+																`;
+													$.ajax({
+														url: "php/load_replies.php",
+														type: "POST",
+														data: { parent_comment_id: element.id },
+														success: function (data) {
+															let result = JSON.parse(data);
+															
+														}
+													});
+									
+													CommentList += `    </div> 
+																</div>`;
+												});
+												
+											}
+										});
+						
+										CommentList += `    </div> 
+													</div>`;
+									});
+								},
+							});
 
+						
+						});
+						CommentList += `    </div> 
+						</div>`;
+						if (
+							normalizeWhitespace(CommentList) !==
+							normalizeWhitespace(commentContainer.innerHTML)
+						) {
+							// Your logic here
+							commentContainer.innerHTML = CommentList;
+							// console.log(normalizeWhitespace(CommentList));
+							// console.log(normalizeWhitespace(commentContainer.innerHTML));
+							// console.log("reassign");
+						}
+					},
+				});
+
+			
+			});
+	
 		},
 	});
 };
 
 setInterval(() => {
 	loadComments();
-}, 500);
+}, 5000);
 
 //Assign Parent Id Value
-function assignParentId(e){
-	commentInput.value = ''
+function assignParentId(e) {
+	commentInput.value = "";
 	ParentCommentId = e.target.dataset.cmId;
-	let CommentUser = e.target.parentElement.previousElementSibling.children[0].children[0].innerHTML
+	let CommentUser =
+		e.target.parentElement.previousElementSibling.children[0].children[0]
+			.innerHTML;
 	console.log(CommentUser);
-	console.log("ParentCommentId : ",ParentCommentId)
-	commentInput.value = CommentUser
+	console.log("ParentCommentId : ", ParentCommentId);
+	commentInput.value = CommentUser;
 }
 
 function normalizeWhitespace(str) {
-	return str.replace(/\s+/g, ' ').trim();
-  }
+	return str.replace(/\s+/g, " ").trim();
+}
+
+//see Replys
+function seeReplies(e) {
+    console.log()
+    e.target.parentElement.parentElement.parentElement.nextElementSibling.classList.toggle('d-none')
+}
--- mandalar/js/product-detail.js
+++ mandalar/js/product-detail.js
@@ -212,8 +212,3 @@ $(document).ready(function() {
 
 });
 
-//see Replys
-function seeReplies(e) {
-    console.log()
-    e.target.parentElement.parentElement.parentElement.nextElementSibling.classList.toggle('d-none')
-}
